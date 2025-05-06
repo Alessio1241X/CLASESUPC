e088393b4583d8df2a8bf1bec9073570f5939627
Alessio1241X <aleccasani2007@gmail.com>
2025-05-06 11:46:35 -0500

    Update EJERCICIOCUADRADO1.cpp

--- EJERCICIOCUADRADO1.cpp
+++ EJERCICIOCUADRADO1.cpp
@@ -12,6 +12,11 @@ for (int i = 0; i <= N; i++) {
 	for (int j = 0; j <= N; j++) {
 		if (i == j || j == 0 ||i==N||i==0|| j== N) {
 			cout << "* ";
+			// i==j-> diagonal
+			// j==0 -> vertical izquierda
+			// i==N -> horizontal inferior
+			// i==0 -> horizontal superior
+			// j==N -> vertical derecha
 		}
 		else {
 			cout << "  ";
