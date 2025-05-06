b3bf97f7ad000e85f53c820680d8e05887891db1
Alessio1241X <aleccasani2007@gmail.com>
2025-05-06 12:33:18 -0500

    Add files via upload

---
+++ RETO6.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+#define N 10
+#define M 10
+int main() {
+
+	int numero;
+
+
+	for (int i = 0; i <= N; i++) {
+		for (int j = 0; j <= N; j++) {
+			if (i == j || j == 0 ||j==8|| i == N || i == 0 || j == N) {
+				cout << "* ";
+			// i==j-> diagonal
+			// j==0 -> vertical izquierda
+			// i==N -> horizontal inferior
+			// i==0 -> horizontal superior
+			// j==N -> vertical derecha
+			}
+			else {
+				cout << "  ";
+			}
+		}
+		cout << endl;
+	}
+	return 0;
+}
